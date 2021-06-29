27120caac65dc415a5df1733afe4d8fdfab7e851
turemusic <turemusic@protonmail.com>
2021-06-30 00:25:56 +0800

    add image

---
+++ MyRayTracing/Project1/Image.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class Image
+{
+public:
+	Image(int image_width, int image_height);
+	~Image();
+	int image_width = 256;
+	int image_height = 256;
+
+
+private:
+
+};
