80f5502eaa8828dae104533e875aeaddb94f0000
HugoTed <zarkfair@live.com>
2021-07-02 22:25:53 +0800

    add hittable

--- MyRayTracing/Project1/main.cpp
+++ MyRayTracing/Project1/main.cpp
@@ -1,8 +1,57 @@
-#include <iostream>
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
 
+#include <iostream>
+//P(t) = A + tb
+//A is ray origin
+//b is ray direction
+//t is a real number
+double hit_sphere(const point3& center, double radius, const ray& r) {
+	vec3 oc = r.origin() - center; //A-C
+	auto a = r.direction().length_squared(); //b 
+	auto half_b = dot(oc, r.direction());
+	auto c = oc.length_squared() - radius * radius;
+	auto discriminant = half_b * half_b - a * c;
+	if (discriminant < 0) {
+		return -1.0;
+	}
+	else {
+		return (-half_b - sqrt(discriminant)) / a;
+	}
+}
+
+//±³¾°½¥±ä
+color ray_color(const ray& r) {
+	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+	if (t > 0.0) {
+		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+		return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+	}
+	vec3 unit_direction = unit_vector(r.direction());
+	t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 int main() {
-	const int image_width = 256;
-	const int image_height = 256;
+
+	//Image
+
+	const auto aspect_ratio = 16 / 9;
+	const int image_width = 400;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+	//Camera
+
+	auto viewport_height = 2.0;
+	auto viewport_width = aspect_ratio * viewport_height; //3.56
+	auto focal_length = 1.0;
+
+	auto origin = point3(0, 0, 0);
+	auto horizontal = vec3(viewport_width, 0, 0);
+	auto vertical = vec3(0, viewport_height, 0);
+	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+	//Render
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
@@ -11,15 +60,11 @@ int main() {
 		std::cerr << "\rScanlines remaining:" << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i)
 		{
-			auto r = double(i) / (image_width - 1);
-			auto g = double(j) / (image_height - 1);
-			auto b = 0.25;
-
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
-
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			auto u = double(i) / (image_width - 1);
+			auto v = double(j) / (image_height - 1);
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+			color pixel_color = ray_color(r);
+			write_color(std::cout, pixel_color);
 		}
 	}
 	std::cerr << "\nDone.\n";
